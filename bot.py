bcfca667717e546d7c9a2b1d3b190f390221d8f9
unknown <sigur.adrien@gmail.com>
2026-01-07 09:41:41 +0100

    serv discord with hours

--- bot.py
+++ bot.py
@@ -1,52 +1,54 @@
 import discord
-import datetime
 from discord.ext import commands
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.cron import CronTrigger 
 import pytz
+from datetime import datetime
 
-TOKEN = "" #Token de l api discord 
-CHANNEL_ID =  # ID du salon où envoyer le message
-ROLE_ID= # ID du role ou envoyer le message 
+TOKEN = "env.file"
+CHANNEL_ID = 1414258422379053066 # ID du salon où envoyer le message
+ROLE_ID= 1398011699528728776
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-now = datetime.datetime.now().strftime("%d/%m/%Y")
 
 scheduler = AsyncIOScheduler(timezone=pytz.timezone("Europe/Paris"))
 
+# --- Fonctions de signature ---
 async def message_matin():
     channel = bot.get_channel(CHANNEL_ID)
     if channel:
-        await channel.send(f" **  ☀️ SIGNATURE DU MATIN  ☀️ !!!** <@&{ROLE_ID}>  {now}")
-
-
+        time_str = datetime.now().strftime("%d/%m/%Y")
+        await channel.send(f"**☀️ SIGNATURE DU MATIN ({time_str})☀️ ** <@&{ROLE_ID}>")
 
 async def message_aprem():
     channel = bot.get_channel(CHANNEL_ID)
     if channel:
-        await channel.send(f"** 🌕  SIGNATURE CET APREM  🌕  !!! ** <@&{ROLE_ID}> {now}")
+        time_str = datetime.now().strftime("%d/%m/%Y")
+        await channel.send(f"**🌖 SIGNATURE CET APREM ({time_str})🌖 ** <@&{ROLE_ID}>")
+
+# async def test():
+#     channel = bot.get_channel(CHANNEL_ID)
+#     if channel:
+#         time_str = datetime.now().strftime("%d/%m/%Y")
+#         await channel.send(f"test {time_str}")
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-
-    # --------- CRON LUNDI ---------
-    # Lundi 9h30
+    
+    # --- CRON LUNDI ---
     scheduler.add_job(message_matin, CronTrigger(day_of_week="mon", hour=9, minute=30))
-
-    # Lundi 13h30
     scheduler.add_job(message_aprem, CronTrigger(day_of_week="mon", hour=13, minute=30))
 
-    # --------- CRON MARDI → VENDREDI ---------
-    # Du mardi au vendredi 9h00
+    # --- CRON MARDI - VENDREDI ---
     scheduler.add_job(message_matin, CronTrigger(day_of_week="tue-fri", hour=9, minute=0))
-
-    # Du mardi au vendredi 13h00
-    scheduler.add_job(message_aprem,  CronTrigger(day_of_week="tue-fri", hour=13, minute=30))
-
+    scheduler.add_job(message_aprem, CronTrigger(day_of_week="tue-fri", hour=13, minute=30))
+    
+    # --- JOB DE TEST (DÉSACTIVÉ) ---
+    # scheduler.add_job(test, CronTrigger(day_of_week="tue", hour=11, minute=48))
+    
     scheduler.start()
-
 
 bot.run(TOKEN)
